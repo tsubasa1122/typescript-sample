667531b2749bdcb80394f11142e285de5697a83c
tsubasa1122 <nakanot@open8.com>
2020-07-14 01:17:34 +0900

    create tuple

---
+++ tuple.ts
@@ -0,0 +1,14 @@
+const person: {
+  name: string;
+  age: number;
+  role: [integer, string];
+} = {
+  name: 'nakano',
+  age: 30,
+  role: [2, 'author']
+};
+
+// person.role.push("admin");
+// person.role[1] = 10;
+
+// person.role = [0, "admin", "string"];

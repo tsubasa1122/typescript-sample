5762c56fa456aa7574ece7d69fa73965dc98f978
tsubasa1122 <nakanot@open8.com>
2020-07-25 00:05:10 +0900

    learn inheritance

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,29 @@ class Department {
   }
 }
 
-const accounting = new Department("d1", "Accounting");
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const accounting = new ITDepartment("d1", ["Accounting"]);
 accounting.addEmployee("Max");
 accounting.addEmployee("Tom");
 // accounting.employees[0] = "Lee";

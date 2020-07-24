bccb932a981783539aa1c4a5c8f5de4003954b60
tsubasa1122 <nakanot@open8.com>
2020-07-24 23:26:03 +0900

    learn rest parameter

--- src/app.ts
+++ src/app.ts
@@ -12,3 +12,13 @@ const person = {
 const copidPerson = {
   ...person,
 };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+
+console.log(addedNumbers);

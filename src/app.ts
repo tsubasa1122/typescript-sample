59ca5f62b4fe1b5b0a1766e00c33d7b6aa83db29
tsubasa1122 <nakanot@open8.com>
2020-07-26 23:56:17 +0900

    type gurd

--- src/app.ts
+++ src/app.ts
@@ -19,3 +19,48 @@ const e1: ElevatedEmploee = {
 type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
+
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log("車を運転中...")
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("トラックを運転中...")
+  }
+
+  loadCargo(amount: number) {
+    console.log("荷物を載せています..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);

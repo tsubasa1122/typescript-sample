7f46d26c1197f09693bda7f0ace76a166f7b8fd8
tsubasa1122 <nakanot@open8.com>
2020-08-02 00:01:04 +0900

    generic class

--- src/app.ts
+++ src/app.ts
@@ -25,3 +25,26 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 };
 
 extractAndConvert({ name: 'Max' }, "name");
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Data1");
+textStorage.addItem("Data2");
+textStorage.removeItem("Data1");
+
+console.log(textStorage.getItems);

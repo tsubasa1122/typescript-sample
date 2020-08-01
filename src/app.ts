2c843b2a8f9d60e83e3909bdbfaed0643da0a019
tsubasa1122 <nakanot@open8.com>
2020-08-01 23:51:31 +0900

    advanced generic

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 };
 
 const mergedOjb = merge({ name: 'Max' }, { age: 30 });
 console.log(mergedOjb.age);
+
+interface Lengthy {
+  length: number;
+};
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = '値がありません';
+  if (element.length > 0) {
+    descriptionText = '値は' + element.length + '個です。';
+  }
+  return [element, descriptionText];
+};
+
+console.log(countAndDescribe(['sports', 'cooking']));
+
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return 'Value: ' + obj[key];
+};
+
+extractAndConvert({ name: 'Max' }, "name");

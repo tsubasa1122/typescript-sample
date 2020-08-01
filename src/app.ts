8e1432ca93dd9c2c1bc5eac0f03c687ae5b8d403
tsubasa1122 <nakanot@open8.com>
2020-08-02 00:08:24 +0900

    utility type

--- src/app.ts
+++ src/app.ts
@@ -48,3 +48,21 @@ textStorage.addItem("Data2");
 textStorage.removeItem("Data1");
 
 console.log(textStorage.getItems);
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+};
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Max', 'Anna'];
+
+// names.push('Manu');

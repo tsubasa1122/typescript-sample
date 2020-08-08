7bd262826fa1c44b22069e57cb4e549cd5ca07a7
tsubasa1122 <nakanot@open8.com>
2020-08-08 23:56:53 +0900

    decorator実装

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,18 @@ function Logger() {
   }
 }
 
-@Logger()
+function WithTemplate(template: string, hookId: string) {
+  return function (_: Function) {
+    const hookEl = document.getElementById(hookId);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+    }
+  }
+}
+// @Logger()
+@WithTemplate('<h1>Personオブジェクト</h1>', "app")
 class Person {
   name = 'Max';
-
   constructor() {
     console.log("Personオブジェクトを作成中....");
   }

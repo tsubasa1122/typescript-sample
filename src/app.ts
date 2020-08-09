e1fa29cd21cdde9264c1acc5bb04f067a277efae
tsubasa1122 <nakanot@open8.com>
2020-08-09 18:38:58 +0900

    decoratorによるクラスの変更

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,21 @@ function Logger() {
 }
 
 function WithTemplate(template: string, hookId: string) {
-  return function (_: Function) {
-    const hookEl = document.getElementById(hookId);
-    if (hookEl) {
-      hookEl.innerHTML = template;
+  return function <T extends { new(...args: any[]): { name: string } }>(originalConstructor: T) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("テンプレート表示中...");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
     }
   }
 }
-// @Logger()
+@Logger()
 @WithTemplate('<h1>Personオブジェクト</h1>', "app")
 class Person {
   name = 'Max';
@@ -25,3 +32,53 @@ class Person {
 const persons = new Person();
 
 console.log(persons);
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('property デコレータ');
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor デコレータ');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('Method デコレータ');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Parameter デコレータ');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('不正な価格です。 - 0以下は設定できません。')
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
